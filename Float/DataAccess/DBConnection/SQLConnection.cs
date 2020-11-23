6498737b14a8eba2d3e3f3ad691065d288d4a08e
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2020-11-23 22:06:46 +0800

    - - added dataaccess for mysql

--- Float/DataAccess/DBConnection/SQLConnection.cs
+++ Float/DataAccess/DBConnection/SQLConnection.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using MySql.Data.MySqlClient;
-using Org.BouncyCastle.Asn1.Mozilla;
 
 namespace DataAccess
 {
     public static class SQLConnection
     {
-        public static const string connectionString = @"server=localhost;userid=root;password=1234;database=simplecrud";
+        public const string connectionString = @"server=localhost;userid=root;password=1234;database=simplecrud";
        
         public static void InitializeDBConnection()
         {
