64e75579300c469c81dbe77d9b4fa811aafbf058
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2020-11-30 20:16:03 +0800

    - - added observablecollection to show usercontrol on right screen

--- Float/Float/Component/Dashboard/MainDashboardViewModel.cs
+++ Float/Float/Component/Dashboard/MainDashboardViewModel.cs
@@ -1,14 +1,48 @@
-﻿using System;
+﻿using Float.BaseModel;
+using Float.Command;
+using Float.Component.Home;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Float.Components.Dashboard
 {
-    public class MainDashboardViewModel
+    public class MainDashboardViewModel : BaseViewModel
     {
+        public MainDashboardViewModel()
+        {
+            ShowRightUserControls = new ObservableCollection<object>();
+        }
+
+        private ObservableCollection<object> showRightUserControls;
+        public ObservableCollection<object> ShowRightUserControls 
+        
+        {
+            get => showRightUserControls;
+            set
+            {
+                showRightUserControls = value;
+                RaisedPropertyChanged(nameof(ShowRightUserControls));
+            }
+        }
+
+        public ICommand OpenHomeControlCommand => new DelegateCommand(OpenHomeControl);
 
 
+        private void ShowControlsOnRight(object window)
+        {
+            ShowRightUserControls.Clear();
+            ShowRightUserControls.Add(window);
+        }
+
+        private void OpenHomeControl()
+        {
+            HomeView homeView = new HomeView();
+            ShowControlsOnRight(homeView);
+        }
     }
 }
