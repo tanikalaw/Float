230ba27bde4b8e5c0f05cbb1e386336d0a0ea874
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2021-03-23 08:36:50 +0800

    - -added mediator classes

---
+++ Float/Float/ViewModelMediator/IMediator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Float.ViewModelMediator
+{
+    interface IMediator
+    {
+        void Register(object vm);
+        void NotifyViewModel(string viewModel, string message, object args);
+
+    }
+}
---
+++ Float/Float/ViewModelMediator/Mediator.cs
@@ -0,0 +1,27 @@
+﻿using Float.BaseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Float.ViewModelMediator
+{
+    public class Mediator : IMediator
+    {
+
+        
+
+        public void NotifyViewModel(string viewModel, string message, object args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(object vm)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
